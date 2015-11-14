15542dec1379ccd56b3988b5ac0907c387e9ef3e
Denis <prohibiti@gmail.com>
2015-11-14 13:12:56 +0200

    load_pages works fine

--- load_games.py
+++ load_games.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from urllib.request import urlopen
@@ -11,54 +11,53 @@ from bs4 import BeautifulSoup
 wdir = os.path.dirname(os.path.realpath(__file__))
 # pages directory
 pdir = os.path.join(wdir, 'pages')
+main_url = 'http://rugame.mobi/game/'
+categories = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 6920, 7169, 7135]
 
 
-def load_pages(number_of_pages):
-    req_url = 'http://rugame.mobi/game/'
-    cat = [range(3, 14)]
-    cat.extend([6920, 7169, 7135])
+def load_pages(category):
+    # creating category directory
+    catdir = os.path.join(pdir, str(category))
 
-    cat = [6920,]#TEMP
+    if not os.path.isdir(catdir):
+        os.mkdir(catdir)
 
-    for i in cat:
-        # creating category directory
-        catdir = os.path.join(pdir, str(i))
+    cat_url = main_url + str(category) + '/'
+    response = urlopen(cat_url)
 
-        if not os.path.isdir(catdir):
-            os.mkdir(catdir)
+    # find how much pages does category have
+    first_page = response.read()
 
-        url = req_url + str(i)
+    with open(os.path.join(catdir, '1.html'), 'wb') as f:
+        f.write(first_page)
+
+    s = BeautifulSoup(first_page)
+    res = s.find_all('div', attrs={'class': 'page'})
+    s = BeautifulSoup(str(res))
+    res = s.find_all('a')
+    link = str(res[-2])
+    nop = re.search('>(.*)<', link)
+    num_of_pages = int(nop.group(1))
+
+    # load all the pages
+    for i in range(2, num_of_pages + 1):
+        page_path = os.path.join(catdir, str(i) + '.html')
+        url = cat_url + str(i) + '/'
         response = urlopen(url)
-
-        # find how much pages does category have
-        first_page = response.read()
-
-        with open(os.path.join(catdir, '1.html'), 'w') as f:
-            f.write(first_page)
-
-        exit()
-        s = BeautifulSoup(first_page)
-        res = s.find_all('div', attrs={'class': 'page'})
-        s = BeautifulSoup(str(res))
-        res = s.find_all('a')
-        link = str(res[-2])
-        nop = re.search('>(.*)<', link)
-        num_of_pages = int(nop.group(1))
-    
-        # load all the pages
-        for j in range(2, num_of_pages + 1):
-            #print 'loading page: ', str(j)
-            page_name = os.path.join(catdir, str(j) + '.html')
-            with open(page_name, 'w') as f:
-                f.write(response.read())
+        with open(page_path, 'wb') as f:
+            f.write(response.read())
     return
 
 
-#def load_songs(page):
-    #'''load songs from free music archive'''
+def load_games_pages(category):
+    '''load games pages from rugame.mobi'''
 
-    #with open('./pages/Free Music Archive: Classical' + str(page), 'r') as f:
-        #s = BeautifulSoup(f.read())
+    cat_dir = os.path.join(pdir, category)
+    pages = os.listdir(cat_dir)
+    for page in pages:
+        page_path = os.path.join(cat_dir, page)
+        with open(page_path, 'r') as f:
+            s = BeautifulSoup(f.read())
 
     #res = s.find_all('div', attrs={'class': 'play-item'})
     #for song_info in res:
@@ -86,17 +85,15 @@ def load_pages(number_of_pages):
 
             #with open(filename, 'w') as f:
                 #f.write(song.read())
-    #return
+    return
 
 
 def main():
-    load_pages(140)
+    load_games_pages(6920)
     exit()
-
-    start = 5
-
-    for i in range(start, start + 1):
-        load_songs(i)
+    for cat in categories:
+        #load_pages(cat)
+        load_games_pages(cat)
     return
 
 
